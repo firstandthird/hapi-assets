9b2e06117abece48c249fc81ac825811b887757f
Greg Allen <me@jga.me>
2015-09-23 09:27:46 -0700

    initial commit

---
+++ helpers/asset.js
@@ -0,0 +1,7 @@
+module.exports = function() {
+
+  var plugin = this;
+
+  return 'yep';
+
+};
---
+++ index.js
@@ -0,0 +1,41 @@
+var _ = require('lodash');
+
+var defaults = {
+  endpoint: '/',
+  path: ''
+};
+
+exports.register = function(server, options, next) {
+
+  options = _.defaults(options, defaults);
+  var plugin = {
+    server: server,
+    options: options,
+    extensions: {}
+  };
+
+  var addExtension = function(extension, fns, options) {
+    if (plugin.extensions[extension]) {
+      throw new Error(extension + ' extension already exists');
+    }
+    if (typeof fns === 'function') {
+      fns = [fns];
+    }
+    plugin.extensions[extension] = {
+      options: options,
+      fns: fns
+    };
+  };
+
+  server.expose('getAsset', require('./helpers/asset').bind(plugin));
+  server.expose('addExtension', addExtension);
+
+  require('./lib/handler')(server, plugin);
+
+  next();
+
+};
+
+exports.register.attributes = {
+  name: 'assets'
+};
